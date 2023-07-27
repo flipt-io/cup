57b40d4cfeb0ffed805a8ddce7410f5c6f991e38
George MacRorie <me@georgemac.com>
2023-07-27 18:55:03 +0100

    feat(controllers/flipt.io/v1alpha): add controller
    parent 8ff2d36ae0974c5bd1e9ef472cad39a1767058e4
    author George MacRorie <me@georgemac.com> 1690480503 +0100
    committer George MacRorie <me@georgemac.com> 1690539890 +0100
    gpgsig -----BEGIN PGP SIGNATURE-----
    
     iQIzBAABCAAdFiEENbRPu7pEDSB0y7knsI7LqcqwVWgFAmTDl3IACgkQsI7Lqcqw
     VWiXBRAAq+ZGrsNnB997PgvPxQQ5Ca7guU6hjjU+VIoOX4tESYooRDs8nny/oYhl
     rsKJUFnrenIJ4YYBMVqiGfAcTYvO2/Kzjb8afFDmhg8+zWqS6wGrBLuNU+g3U/o+
     7XHX9AHBq4su5jF23FzJo0fuiOj2iY7EoXhI5S3V65oZfoek5ZMjAgrNzYkzxp1R
     JT0tFO/qN/8opBeLXKqfC+O6VzQSj/TY9x9gdktpOUPtTsVlE2k8Vt6uwpQ/D8h+
     cIZw+lpKGlPZesNWVfP5PwvBtv3bBKzfyntyGu7S9Vo7QIWAv+UBDVAp4vVJuaIG
     EalECpLwfQ5I/o0ERayZa++rHyXS3m5yC7fQioUsLxc72mpfkxuGHiL5jEfMh/hH
     gh8NgZQBabvp5EyFencEEQXHHE34btpXzMtNFhY68IFa6FC1f1b2esb0TWsyRCga
     7WpiyG26Y+F/4rDI/jYCVbCdm9vWB3QG9UfHTpiz+ABAbSO7i/mmxiHQRarATq3v
     ewuhybxB8H+DPt+Mqej4uz3UqIuJLsGxLMEOKsnCyEDpN/PdhaDVKv5GmCuPwCZw
     7Y0+f/VdRZkd8tpRlf+xRR5DASrX3fyf1ReRhezXUgPwiN6ci57Iy/oc8jcM2YNC
     tFyLIUkdgBwgxGDI+K1jt9kKugi/cZN7BND2a8gv4+UICx2r/Ks=
     =YqIO
     -----END PGP SIGNATURE-----
    
    feat(controllers/flipt.io/v1alpha): add Flag kind controller
    
    feat(controllers/flipt.io/v1alpha): add Segment kind controller
    
    feat(cup/ctl): add identation to json output

---
+++ ext/controllers/flipt.io/v1alpha1/cmd/flipt/flag.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"go.flipt.io/cup/ext/controllers/flipt.io/v1alpha1/pkg/ext"
+	"go.flipt.io/cup/pkg/api/core"
+	"go.flipt.io/cup/pkg/encoding"
+	"gopkg.in/yaml.v2"
+)
+
+type flagController struct{}
+
+func (c *flagController) Get(ctx context.Context, namespace, name string, enc encoding.TypedEncoder[Flag]) error {
+	defer func() {
+		if err := recover(); err != nil {
+			panic(fmt.Errorf("Get: %v", err))
+		}
+	}()
+
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		for _, f := range document.Flags {
+			if f.Key == name {
+				if err := enc.Encode(&Flag{
+					Namespace: namespace,
+					flag:      f,
+				}); err != nil {
+					return err
+				}
+
+				return nil
+			}
+		}
+
+		return fmt.Errorf("flag %s/%s: %w", namespace, name, ErrNotFound)
+	})
+}
+
+func (c *flagController) List(ctx context.Context, namespace string, enc encoding.TypedEncoder[Flag]) error {
+	defer func() {
+		if err := recover(); err != nil {
+			panic(fmt.Errorf("List: %v", err))
+		}
+	}()
+
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		for _, f := range document.Flags {
+			if err := enc.Encode(&Flag{
+				Namespace: namespace,
+				flag:      f,
+			}); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func (c *flagController) Put(ctx context.Context, namespace, name string, flag *Flag) error {
+	dir := os.DirFS(".")
+	return walkDocuments(dir, func(path string, document *ext.Document) error {
+		if document.Namespace != string(flag.Namespace) {
+			return nil
+		}
+
+		var found bool
+		for i, f := range document.Flags {
+			if found = f.Key == string(flag.flag.Key); found {
+				document.Flags[i] = flag.flag
+				break
+			}
+		}
+
+		if !found {
+			document.Flags = append(document.Flags, flag.flag)
+		}
+
+		fi, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		}
+
+		defer fi.Close()
+
+		return yaml.NewEncoder(fi).Encode(document)
+	})
+}
+
+func (c *flagController) Delete(ctx context.Context, namespace, name string) error {
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		var found bool
+		for i, f := range document.Flags {
+			if f.Key != name {
+				continue
+			}
+
+			document.Flags = append(document.Flags[:i], document.Flags[i+1:]...)
+
+			found = true
+		}
+
+		if !found {
+			return nil
+		}
+
+		fi, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		}
+
+		defer fi.Close()
+
+		return yaml.NewEncoder(fi).Encode(document)
+	})
+}
+
+type Flag struct {
+	Namespace string
+	flag      *ext.Flag
+}
+
+func (f *Flag) UnmarshalJSON(v []byte) error {
+	var resource core.Resource
+	if err := json.Unmarshal(v, &resource); err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(resource.Spec, &f.flag); err != nil {
+		return err
+	}
+
+	f.Namespace = resource.Metadata.Namespace
+	f.flag.Key = resource.Metadata.Name
+	f.flag.Name = resource.Metadata.Name
+
+	return nil
+}
+
+func (f *Flag) MarshalJSON() (_ []byte, err error) {
+	resource := core.Resource{
+		APIVersion: "flipt.io/v1alpha1",
+		Kind:       "Flag",
+		Metadata: core.NamespacedMetadata{
+			Namespace: f.Namespace,
+			Name:      f.flag.Key,
+		},
+	}
+
+	flag := *f.flag
+	flag.Key = ""  // key will be carried by the resource name
+	flag.Name = "" // key will be carried by the resource name
+
+	resource.Spec, err = json.Marshal(&f.flag)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling flag: %w", err)
+	}
+
+	return json.Marshal(&resource)
+}
---
+++ ext/controllers/flipt.io/v1alpha1/cmd/flipt/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+
+	"go.flipt.io/cup/ext/controllers/flipt.io/v1alpha1/pkg/ext"
+
+	"github.com/gobwas/glob"
+	sdk "go.flipt.io/cup/sdk/controller/go"
+	"golang.org/x/exp/slog"
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	ErrNotFound = errors.New("not found")
+)
+
+const (
+	indexFile = ".flipt.yml"
+	defaultNs = "default"
+)
+
+func main() {
+	cli := sdk.NewCLI()
+	cli.RegisterKind("Flag", sdk.NewKindController[Flag](&flagController{}))
+	cli.RegisterKind("Segment", sdk.NewKindController[Segment](&segmentController{}))
+	cli.Run(context.Background(), os.Args...)
+}
+
+var errFinish = errors.New("finish")
+
+func walkDocuments(source fs.FS, fn func(path string, document *ext.Document) error) error {
+	paths, err := listStateFiles(source)
+	if err != nil {
+		return err
+	}
+
+	for _, p := range paths {
+		fi, err := source.Open(p)
+		if err != nil {
+			return err
+		}
+
+		defer fi.Close()
+
+		doc := new(ext.Document)
+		if err := yaml.NewDecoder(fi).Decode(doc); err != nil {
+			return err
+		}
+
+		if doc.Namespace == "" {
+			doc.Namespace = "default"
+		}
+
+		if err := fn(p, doc); err != nil {
+			if errors.Is(err, errFinish) {
+				return nil
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+// FliptIndex represents the structure of a well-known file ".flipt.yml"
+// at the root of an FS.
+type FliptIndex struct {
+	Version string   `yaml:"version,omitempty"`
+	Include []string `yaml:"include,omitempty"`
+	Exclude []string `yaml:"exclude,omitempty"`
+}
+
+func listStateFiles(source fs.FS) ([]string, error) {
+	// This is the default variable + value for the FliptIndex. It will preserve its value if
+	// a .flipt.yml can not be read for whatever reason.
+	idx := FliptIndex{
+		Version: "1.0",
+		Include: []string{
+			"**features.yml", "**features.yaml", "**.features.yml", "**.features.yaml",
+		},
+	}
+
+	// Read index file
+	inFile, err := source.Open(indexFile)
+	if err == nil {
+		if derr := yaml.NewDecoder(inFile).Decode(&idx); derr != nil {
+			return nil, fmt.Errorf("yaml: %w", derr)
+		}
+	}
+
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return nil, err
+		} else {
+			slog.Debug("index file does not exist, defaulting...", slog.String("file", indexFile), "error", err)
+		}
+	}
+
+	var includes []glob.Glob
+	for _, g := range idx.Include {
+		glob, err := glob.Compile(g)
+		if err != nil {
+			return nil, fmt.Errorf("compiling include glob: %w", err)
+		}
+
+		includes = append(includes, glob)
+	}
+
+	filenames := make([]string, 0)
+	if err := fs.WalkDir(source, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		for _, glob := range includes {
+			if glob.Match(path) {
+				filenames = append(filenames, path)
+				return nil
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	if len(idx.Exclude) > 0 {
+		var excludes []glob.Glob
+		for _, g := range idx.Exclude {
+			glob, err := glob.Compile(g)
+			if err != nil {
+				return nil, fmt.Errorf("compiling include glob: %w", err)
+			}
+
+			excludes = append(excludes, glob)
+		}
+
+	OUTER:
+		for i := range filenames {
+			for _, glob := range excludes {
+				if glob.Match(filenames[i]) {
+					filenames = append(filenames[:i], filenames[i+1:]...)
+					continue OUTER
+				}
+			}
+		}
+	}
+
+	return filenames, nil
+}
