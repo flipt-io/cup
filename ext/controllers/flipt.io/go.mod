5721fbe77e95110ef9d80a7e6bdac4e9f77796d0
George MacRorie <me@georgemac.com>
2023-07-27 18:55:03 +0100

    feat(controllers/flipt.io/v1alpha): add controller
    parent 8ff2d36ae0974c5bd1e9ef472cad39a1767058e4
    author George MacRorie <me@georgemac.com> 1690480503 +0100
    committer George MacRorie <me@georgemac.com> 1690539890 +0100
    gpgsig -----BEGIN PGP SIGNATURE-----
    
     iQIzBAABCAAdFiEENbRPu7pEDSB0y7knsI7LqcqwVWgFAmTDl3IACgkQsI7Lqcqw
     VWiXBRAAq+ZGrsNnB997PgvPxQQ5Ca7guU6hjjU+VIoOX4tESYooRDs8nny/oYhl
     rsKJUFnrenIJ4YYBMVqiGfAcTYvO2/Kzjb8afFDmhg8+zWqS6wGrBLuNU+g3U/o+
     7XHX9AHBq4su5jF23FzJo0fuiOj2iY7EoXhI5S3V65oZfoek5ZMjAgrNzYkzxp1R
     JT0tFO/qN/8opBeLXKqfC+O6VzQSj/TY9x9gdktpOUPtTsVlE2k8Vt6uwpQ/D8h+
     cIZw+lpKGlPZesNWVfP5PwvBtv3bBKzfyntyGu7S9Vo7QIWAv+UBDVAp4vVJuaIG
     EalECpLwfQ5I/o0ERayZa++rHyXS3m5yC7fQioUsLxc72mpfkxuGHiL5jEfMh/hH
     gh8NgZQBabvp5EyFencEEQXHHE34btpXzMtNFhY68IFa6FC1f1b2esb0TWsyRCga
     7WpiyG26Y+F/4rDI/jYCVbCdm9vWB3QG9UfHTpiz+ABAbSO7i/mmxiHQRarATq3v
     ewuhybxB8H+DPt+Mqej4uz3UqIuJLsGxLMEOKsnCyEDpN/PdhaDVKv5GmCuPwCZw
     7Y0+f/VdRZkd8tpRlf+xRR5DASrX3fyf1ReRhezXUgPwiN6ci57Iy/oc8jcM2YNC
     tFyLIUkdgBwgxGDI+K1jt9kKugi/cZN7BND2a8gv4+UICx2r/Ks=
     =YqIO
     -----END PGP SIGNATURE-----
    
    feat(controllers/flipt.io/v1alpha): add Flag kind controller
    
    feat(controllers/flipt.io/v1alpha): add Segment kind controller
    
    feat(cup/ctl): add identation to json output

---
+++ ext/controllers/flipt.io/go.mod
@@ -0,0 +1,18 @@
+module go.flipt.io/cup/ext/controllers/flipt.io
+
+go 1.20
+
+require (
+	github.com/gobwas/glob v0.2.3
+	go.flipt.io/cup v0.0.0-00010101000000-000000000000
+	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090
+	gopkg.in/yaml.v2 v2.2.4
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
+
+replace go.flipt.io/cup => ../../../
---
+++ ext/controllers/flipt.io/v1alpha1/cmd/flipt/segment.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"go.flipt.io/cup/ext/controllers/flipt.io/v1alpha1/pkg/ext"
+	"go.flipt.io/cup/pkg/api/core"
+	"go.flipt.io/cup/pkg/encoding"
+	"gopkg.in/yaml.v2"
+)
+
+type segmentController struct{}
+
+func (c *segmentController) Get(ctx context.Context, namespace, name string, enc encoding.TypedEncoder[Segment]) error {
+	defer func() {
+		if err := recover(); err != nil {
+			panic(fmt.Errorf("Get: %v", err))
+		}
+	}()
+
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		for _, s := range document.Segments {
+			if s.Key == name {
+				if err := enc.Encode(&Segment{
+					Namespace: namespace,
+					segment:   s,
+				}); err != nil {
+					return err
+				}
+
+				return nil
+			}
+		}
+
+		return fmt.Errorf("segment %s/%s: %w", namespace, name, ErrNotFound)
+	})
+}
+
+func (c *segmentController) List(ctx context.Context, namespace string, enc encoding.TypedEncoder[Segment]) error {
+	defer func() {
+		if err := recover(); err != nil {
+			panic(fmt.Errorf("List: %v", err))
+		}
+	}()
+
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		for _, s := range document.Segments {
+			if err := enc.Encode(&Segment{
+				Namespace: namespace,
+				segment:   s,
+			}); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func (c *segmentController) Put(ctx context.Context, namespace, name string, segment *Segment) error {
+	dir := os.DirFS(".")
+	return walkDocuments(dir, func(path string, document *ext.Document) error {
+		if document.Namespace != string(segment.Namespace) {
+			return nil
+		}
+
+		var found bool
+		for i, s := range document.Segments {
+			if found = s.Key == string(segment.segment.Key); found {
+				document.Segments[i] = segment.segment
+				break
+			}
+		}
+
+		if !found {
+			document.Segments = append(document.Segments, segment.segment)
+		}
+
+		fi, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		}
+
+		defer fi.Close()
+
+		return yaml.NewEncoder(fi).Encode(document)
+	})
+}
+
+func (c *segmentController) Delete(ctx context.Context, namespace, name string) error {
+	return walkDocuments(os.DirFS("."), func(path string, document *ext.Document) error {
+		if document.Namespace != namespace {
+			return nil
+		}
+
+		var found bool
+		for i, s := range document.Segments {
+			if s.Key != name {
+				continue
+			}
+
+			document.Segments = append(document.Segments[:i], document.Segments[i+1:]...)
+
+			found = true
+		}
+
+		if !found {
+			return nil
+		}
+
+		fi, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		}
+
+		defer fi.Close()
+
+		return yaml.NewEncoder(fi).Encode(document)
+	})
+}
+
+type Segment struct {
+	Namespace string
+	segment   *ext.Segment
+}
+
+func (s *Segment) UnmarshalJSON(v []byte) error {
+	var resource core.Resource
+	if err := json.Unmarshal(v, &resource); err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(resource.Spec, &s.segment); err != nil {
+		return err
+	}
+
+	s.Namespace = resource.Metadata.Namespace
+	s.segment.Key = resource.Metadata.Name
+	s.segment.Name = resource.Metadata.Name
+
+	return nil
+}
+
+func (s *Segment) MarshalJSON() (_ []byte, err error) {
+	resource := core.Resource{
+		APIVersion: "flipt.io/v1alpha1",
+		Kind:       "Segment",
+		Metadata: core.NamespacedMetadata{
+			Namespace: s.Namespace,
+			Name:      s.segment.Key,
+		},
+	}
+
+	segment := *s.segment
+	segment.Key = ""  // key will be carried by the resource name
+	segment.Name = "" // key will be carried by the resource name
+
+	resource.Spec, err = json.Marshal(&s.segment)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling segment: %w", err)
+	}
+
+	return json.Marshal(&resource)
+}
