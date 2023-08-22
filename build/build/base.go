1dd6a51e109f6ea494bf479adc9b7e2a76e4b900
George MacRorie <me@georgemac.com>
2023-08-22 14:28:24 +0100

    feat(build): add ./build.sh publish target

---
+++ build/build/base.go
@@ -0,0 +1,210 @@
+package build
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"path"
+
+	"dagger.io/dagger"
+	"github.com/containerd/containerd/platforms"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	goBuildCachePath = "/root/.cache/go-build"
+	goModCachePath   = "/go/pkg/mod"
+)
+
+// SupportedPlatforms is the set of platforms cup currently can be
+// build for using these Dagger pipelines.
+var SupportedPlatforms = []dagger.Platform{
+	"linux/amd64",
+	"linux/arm64",
+}
+
+type ContainerFn func(*dagger.Container) *dagger.Container
+
+type Options struct {
+	platforms        []dagger.Platform
+	variantExtension ContainerFn
+}
+
+func defaultOptions() Options {
+	return Options{
+		platforms: SupportedPlatforms,
+		variantExtension: func(c *dagger.Container) *dagger.Container {
+			return c
+		},
+	}
+}
+
+type Option func(*Options)
+
+func SetPlatform(platform dagger.Platform) Option {
+	return func(bo *Options) {
+		bo.platforms = []dagger.Platform{platform}
+	}
+}
+
+func WithVariantExtention(fn ContainerFn) Option {
+	return func(o *Options) {
+		o.variantExtension = fn
+	}
+}
+
+func Base(ctx context.Context, client *dagger.Client, opts ...Option) (*dagger.Container, error) {
+	o := defaultOptions()
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	base := client.Container().
+		From("golang:1.21-alpine3.18").
+		WithEnvVariable("GOCACHE", goBuildCachePath).
+		WithEnvVariable("GOMODCACHE", goModCachePath).
+		WithExec([]string{"apk", "add", "gcc", "build-base"}).
+		WithDirectory("/src", client.Host().Directory("."), dagger.ContainerWithDirectoryOpts{
+			Include: []string{
+				"./build/",
+				"./cmd/",
+				"./pkg/",
+				"./ext/",
+				"./sdk/",
+				"./go.work",
+				"./go.work.sum",
+				"./go.mod",
+				"./go.sum",
+			},
+		}).
+		WithWorkdir("/src")
+
+	sumContents, err := base.File("go.work.sum").Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sum := fmt.Sprintf("%x", sha256.Sum256([]byte(sumContents)))
+	var (
+		cacheGoBuild = client.CacheVolume(fmt.Sprintf("go-build-%s", sum))
+		cacheGoMod   = client.CacheVolume(fmt.Sprintf("go-mod-%s", sum))
+	)
+
+	base = base.
+		WithMountedCache(goBuildCachePath, cacheGoBuild).
+		WithMountedCache(goModCachePath, cacheGoMod)
+
+	type result struct {
+		path   string
+		binary *dagger.File
+	}
+
+	var (
+		group errgroup.Group
+		ch    = make(chan result)
+	)
+
+	for _, name := range []string{"cup", "cupd"} {
+		for _, platform := range o.platforms {
+			var (
+				name     = name
+				platform = platform
+			)
+
+			group.Go(func() error {
+				p := platforms.MustParse(string(platform))
+
+				binary := fmt.Sprintf("bin/%s/%s/%s", p.OS, p.Architecture, name)
+				build, err := base.WithEnvVariable("GOOS", p.OS).
+					WithEnvVariable("GOARCH", p.Architecture).
+					WithExec([]string{"sh", "-c", fmt.Sprintf("go build -o %s ./cmd/%s/*.go", binary, name)}).
+					Sync(ctx)
+				if err != nil {
+					return err
+				}
+
+				ch <- result{
+					path:   binary,
+					binary: build.File(binary),
+				}
+
+				return nil
+			})
+		}
+	}
+
+	go func() {
+		defer close(ch)
+		err = group.Wait()
+	}()
+
+	for result := range ch {
+		base = base.WithFile(result.path, result.binary)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return base.Sync(ctx)
+}
+
+func Variants(ctx context.Context, client *dagger.Client, base *dagger.Container, opts ...Option) (variants []*dagger.Container, err error) {
+	o := defaultOptions()
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	var (
+		group errgroup.Group
+		ch    = make(chan *dagger.Container)
+	)
+
+	for _, platform := range o.platforms {
+		platform := platform
+		group.Go(func() error {
+			container, err := o.variantExtension(Variant(client, base, platform)).
+				Sync(ctx)
+			if err != nil {
+				return err
+			}
+
+			ch <- container
+
+			return nil
+		})
+	}
+
+	go func() {
+		defer close(ch)
+		err = group.Wait()
+	}()
+
+	for container := range ch {
+		variants = append(variants, container)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func Variant(client *dagger.Client, base *dagger.Container, platform dagger.Platform) *dagger.Container {
+	var (
+		p      = platforms.MustParse(string(platform))
+		binDir = path.Join("bin", p.OS, p.Architecture)
+	)
+
+	return client.
+		Container(dagger.ContainerOpts{Platform: platform}).
+		From("alpine:3.18").
+		WithExec([]string{"mkdir", "-p", "/var/run/cupd"}).
+		WithWorkdir("/var/run/cupd").
+		WithFile("/usr/local/bin/cupd", base.File(path.Join(binDir, "cupd"))).
+		WithFile("/usr/local/bin/cup", base.File(path.Join(binDir, "cup"))).
+		WithDefaultArgs(dagger.ContainerWithDefaultArgsOpts{
+			Args: []string{"/usr/local/bin/cupd", "serve", "-api-resources", "/etc/cupd/config"},
+		})
+}
--- build/main.go
+++ build/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"context"
-	"crypto/sha256"
 	"fmt"
-	"log/slog"
 	"os"
 
 	"dagger.io/dagger"
-	"github.com/containerd/containerd/platforms"
 	"github.com/urfave/cli/v2"
+	"go.flipt.io/cup/build/build"
 	"go.flipt.io/cup/build/hack"
 	"go.flipt.io/cup/build/testing"
+	"golang.org/x/exp/slog"
 )
 
 const (
@@ -19,14 +18,92 @@ const (
 	goModCachePath   = "/go/pkg/mod"
 )
 
+var (
+	publishFlags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "registry",
+			Value:   "ghcr.io",
+			EnvVars: []string{"CUP_BUILD_REGISTRY"},
+		},
+		&cli.StringFlag{
+			Name:    "username",
+			Value:   "flipt-io",
+			EnvVars: []string{"CUP_BUILD_USERNAME"},
+		},
+		&cli.StringFlag{
+			Name:    "password",
+			Value:   "",
+			EnvVars: []string{"CUP_BUILD_PASSWORD"},
+		},
+		&cli.StringFlag{
+			Name:    "image-name",
+			Value:   "cup/flipt:latest",
+			EnvVars: []string{"CUP_BUILD_IMAGE_NAME"},
+		},
+	}
+)
+
 func main() {
 	app := &cli.App{
 		Name: "build",
 		Commands: []*cli.Command{
 			{
-				Name: "build",
+				Name: "base",
 				Action: func(ctx *cli.Context) error {
-					return build(ctx.Context)
+					return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+						_, err := build.Base(ctx.Context, client)
+						return err
+					})
+				},
+			},
+			{
+				Name: "image",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "output",
+						Aliases:  []string{"o"},
+						Required: true,
+						Usage:    "output file path for OCI tarball",
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+						variants, err := buildVariants(ctx, client)
+						if err != nil {
+							return err
+						}
+
+						_, err = client.Container().Export(
+							ctx.Context,
+							ctx.String("output"),
+							dagger.ContainerExportOpts{
+								PlatformVariants: variants,
+							},
+						)
+
+						return err
+					})
+				},
+			},
+			{
+				Name:  "publish",
+				Flags: publishFlags,
+				Action: func(ctx *cli.Context) error {
+					return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+						variants, err := buildVariants(ctx, client)
+						if err != nil {
+							return err
+						}
+
+						tag, err := publishVariants(ctx, client, variants)
+						if err != nil {
+							return err
+						}
+
+						slog.Info("Published Image", "tag", tag)
+
+						return nil
+					})
 				},
 			},
 			{
@@ -35,13 +112,36 @@ func main() {
 					{
 						Name: "unit",
 						Action: func(ctx *cli.Context) error {
-							return testUnit(ctx.Context)
+							return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+								// fix tests to just build and run against platform in context
+								base, err := build.Base(ctx.Context, client, build.SetPlatform(platform))
+								if err != nil {
+									return err
+								}
+
+								_, err = base.WithEnvVariable("CGO_ENABLED", "1").
+									WithExec([]string{"go", "test", "-race", "-v", "./..."}).
+									Sync(ctx.Context)
+
+								return err
+							})
 						},
 					},
 					{
 						Name: "integration",
 						Action: func(ctx *cli.Context) error {
-							return withCup(ctx.Context, func(client *dagger.Client, base, cup *dagger.Container, platform dagger.Platform) error {
+							return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+								// fix tests to just build and run against platform in context
+								base, err := build.Base(ctx.Context, client, build.SetPlatform(platform))
+								if err != nil {
+									return err
+								}
+
+								cup, err := build.Variant(client, base, platform).Sync(ctx.Context)
+								if err != nil {
+									return err
+								}
+
 								return testing.Integration(ctx.Context, client, base, cup)
 							})
 						},
@@ -52,93 +152,72 @@ func main() {
 				Name: "hack",
 				Subcommands: []*cli.Command{
 					{
-						Name: "fliptcup:build",
-						Action: func(ctx *cli.Context) error {
-							return withBase(ctx.Context, func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error {
-								cup, err := hack.FliptCup(ctx.Context, client, base, platform)
-								if err != nil {
-									return err
-								}
-
-								_, err = cup.Export(ctx.Context, "fliptcup.tar")
-								return err
-							})
-						},
-					},
-					{
-						Name: "fliptcup:publish",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:    "registry",
-								Value:   "ghcr.io",
-								EnvVars: []string{"CUP_BUILD_REGISTRY"},
-							},
-							&cli.StringFlag{
-								Name:    "username",
-								Value:   "flipt-io",
-								EnvVars: []string{"CUP_BUILD_USERNAME"},
-							},
-							&cli.StringFlag{
-								Name:    "password",
-								Value:   "",
-								EnvVars: []string{"CUP_BUILD_PASSWORD"},
-							},
-							&cli.StringFlag{
-								Name:    "image-name",
-								Value:   "cup/flipt:latest",
-								EnvVars: []string{"CUP_BUILD_IMAGE_NAME"},
-							},
-						},
-						Action: func(ctx *cli.Context) error {
-							var (
-								variants  []*dagger.Container
-								platforms = []dagger.Platform{
-									"linux/amd64",
-									"linux/arm64",
-								}
-								build = func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error {
-									cup, err := hack.FliptCup(ctx.Context, client, base, platform)
-									if err != nil {
-										return err
-									}
-
-									variants = append(variants, cup)
-
-									return nil
-								}
-							)
-
-							for _, platform := range platforms {
-								if err := withBase(ctx.Context, build, withPlatform(platform)); err != nil {
-									return err
-								}
-							}
-
-							return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
-								var (
-									registry = ctx.String("registry")
-									username = ctx.String("username")
-									password = ctx.String("password")
-								)
-
-								tag, err := client.Container().WithRegistryAuth(
-									registry,
-									username,
-									client.SetSecret("registry-password", password),
-								).Publish(ctx.Context,
-									fmt.Sprintf("%s/%s/%s", registry, username, ctx.String("image-name")),
-									dagger.ContainerPublishOpts{
-										PlatformVariants: variants,
+						Name:        "fliptcup",
+						Description: "Build cup images with baked-in flipt controller",
+						Subcommands: []*cli.Command{
+							{
+								Name: "image",
+								Flags: []cli.Flag{
+									&cli.StringFlag{
+										Name:     "output",
+										Aliases:  []string{"o"},
+										Required: true,
+										Usage:    "output file path for OCI tarball",
 									},
-								)
-								if err != nil {
-									return err
-								}
+								},
+								Action: func(ctx *cli.Context) error {
+									return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+										base, err := build.Base(ctx.Context, client)
+										if err != nil {
+											return err
+										}
 
-								slog.Info("Published Image", "tag", tag)
+										variants, err := build.Variants(ctx.Context, client, base, build.WithVariantExtention(
+											hack.FliptController(ctx.Context, client, base),
+										))
+										if err != nil {
+											return err
+										}
 
-								return nil
-							})
+										_, err = client.Container().Export(
+											ctx.Context,
+											ctx.String("output"),
+											dagger.ContainerExportOpts{
+												PlatformVariants: variants,
+											},
+										)
+										return err
+									})
+								},
+							},
+							{
+								Name:  "publish",
+								Flags: publishFlags,
+								Action: func(ctx *cli.Context) error {
+									return withClient(ctx.Context, func(client *dagger.Client, platform dagger.Platform) error {
+										base, err := build.Base(ctx.Context, client)
+										if err != nil {
+											return err
+										}
+
+										variants, err := build.Variants(ctx.Context, client, base, build.WithVariantExtention(
+											hack.FliptController(ctx.Context, client, base),
+										))
+										if err != nil {
+											return err
+										}
+
+										tag, err := publishVariants(ctx, client, variants)
+										if err != nil {
+											return err
+										}
+
+										slog.Info("Published Image", "tag", tag)
+
+										return nil
+									})
+								},
+							},
 						},
 					},
 				},
@@ -152,109 +231,46 @@ func main() {
 	}
 }
 
-func testUnit(ctx context.Context) error {
-	return withBase(ctx, func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error {
-		_, err := base.
-			WithEnvVariable("CGO_ENABLED", "1").
-			WithExec([]string{"go", "test", "-race", "-v", "./..."}).
-			Sync(ctx)
-		return err
-	})
+func publishVariants(ctx *cli.Context, client *dagger.Client, variants []*dagger.Container) (string, error) {
+	var (
+		registry = ctx.String("registry")
+		username = ctx.String("username")
+		password = ctx.String("password")
+	)
+
+	return client.Container().WithRegistryAuth(
+		registry,
+		username,
+		client.SetSecret("registry-password", password),
+	).Publish(ctx.Context,
+		fmt.Sprintf("%s/%s/%s", registry, username, ctx.String("image-name")),
+		dagger.ContainerPublishOpts{
+			PlatformVariants: variants,
+		},
+	)
+
 }
 
-func build(ctx context.Context) error {
-	return withBase(ctx, func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error {
-		_, err := base.
-			WithExec([]string{"go", "install", "./..."}).
-			Sync(ctx)
-		return err
-	})
-}
-
-func withCup(ctx context.Context, fn func(client *dagger.Client, base, cup *dagger.Container, platform dagger.Platform) error, opts ...option) error {
-	return withBase(ctx, func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error {
-		cup, err := client.
-			Container(dagger.ContainerOpts{Platform: platform}).
-			From("alpine:3.18").
-			WithExec([]string{"mkdir", "-p", "/var/run/cupd"}).
-			WithWorkdir("/var/run/cupd").
-			WithFile("/usr/local/bin/cupd", base.File("/usr/local/bin/cupd")).
-			WithFile("/usr/local/bin/cup", base.File("/usr/local/bin/cup")).
-			WithDefaultArgs(dagger.ContainerWithDefaultArgsOpts{
-				Args: []string{"/usr/local/bin/cupd", "serve", "-api-resources", "/etc/cupd/config"},
-			}).
-			Sync(ctx)
-		if err != nil {
-			return err
-		}
-
-		return fn(client, base, cup, platform)
-	}, opts...)
-}
-
-func withBase(ctx context.Context, fn func(client *dagger.Client, base *dagger.Container, platform dagger.Platform) error, opts ...option) error {
-	return withClient(ctx, func(client *dagger.Client, platform dagger.Platform) error {
-		p := platforms.MustParse(string(platform))
-
-		base := client.Container().
-			From("golang:1.21-alpine3.18").
-			WithEnvVariable("GOCACHE", goBuildCachePath).
-			WithEnvVariable("GOMODCACHE", goModCachePath).
-			WithEnvVariable("GOOS", p.OS).
-			WithEnvVariable("GOARCH", p.Architecture).
-			WithExec([]string{"apk", "add", "gcc", "build-base"}).
-			WithMountedDirectory("/src", client.Host().Directory(".")).
-			WithWorkdir("/src")
-
-		sumContents, err := base.File("go.work.sum").Contents(ctx)
-		if err != nil {
-			return err
-		}
-
-		sum := fmt.Sprintf("%x", sha256.Sum256([]byte(sumContents)))
-		var (
-			cacheGoBuild = client.CacheVolume(fmt.Sprintf("go-build-%s", sum))
-			cacheGoMod   = client.CacheVolume(fmt.Sprintf("go-mod-%s", sum))
-		)
-
-		return fn(client, base.
-			WithMountedCache(goBuildCachePath, cacheGoBuild).
-			WithMountedCache(goModCachePath, cacheGoMod).
-			WithExec([]string{"sh", "-c", "go build -o /usr/local/bin/cupd ./cmd/cupd/*.go"}).
-			WithExec([]string{"sh", "-c", "go build -o /usr/local/bin/cup ./cmd/cup/*.go"}),
-			platform,
-		)
-	}, opts...)
-}
-
-type config struct {
-	platform dagger.Platform
-}
-
-type option func(*config)
-
-func withPlatform(platform dagger.Platform) option {
-	return func(c *config) {
-		c.platform = platform
+func buildVariants(ctx *cli.Context, client *dagger.Client, opts ...build.Option) ([]*dagger.Container, error) {
+	base, err := build.Base(ctx.Context, client)
+	if err != nil {
+		return nil, err
 	}
+
+	return build.Variants(ctx.Context, client, base, opts...)
 }
 
-func withClient(ctx context.Context, fn func(client *dagger.Client, platform dagger.Platform) error, opts ...option) error {
+func withClient(ctx context.Context, fn func(client *dagger.Client, platform dagger.Platform) error) error {
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 
-	config := config{}
-	config.platform, err = client.DefaultPlatform(ctx)
+	platform, err := client.DefaultPlatform(ctx)
 	if err != nil {
 		return err
 	}
 
-	for _, opt := range opts {
-		opt(&config)
-	}
-
-	return fn(client, config.platform)
+	return fn(client, platform)
 }
